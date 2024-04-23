4e328043526f079708c06bf1c0bd9b84ecd6a1ab
Kelemen Péter <peter.kelemen456@gmail.com>
2024-04-23 08:19:28 +0200

    asdasd

---
+++ src/main/java/com/esen/bookstore/model/Bookstore.java
@@ -0,0 +1,31 @@
+package com.esen.bookstore.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bookstore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String location;
+    private Double priceModifier;
+    private Double moneyInCashRegister;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Map<Book, Integer> inventory;
+}
