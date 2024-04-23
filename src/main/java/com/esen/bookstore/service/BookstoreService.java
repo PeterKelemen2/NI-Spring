082d4bc5f3e4a02e61540cb920980dbcd3c1b69e
Kelemen Péter <peter.kelemen456@gmail.com>
2024-04-23 09:27:20 +0200

    aaaaaaaaaaaaaa

--- src/main/java/com/esen/bookstore/service/BookstoreService.java
+++ src/main/java/com/esen/bookstore/service/BookstoreService.java
@@ -1,22 +1,65 @@
 package com.esen.bookstore.service;
 
 import com.esen.bookstore.model.Book;
+import com.esen.bookstore.model.Bookstore;
 import com.esen.bookstore.repository.BookstoreRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 @Service
 @RequiredArgsConstructor
 public class BookstoreService {
     private final BookstoreRepository bookstoreRepository;
+    private final BookstoreService bookstoreService;
 
     @Transactional
-    public void removeBookFromInventories(Book book){
+    public void removeBookFromInventories(Book book) {
         bookstoreRepository.findAll()
                 .forEach(bookstore -> {
                     bookstore.getInventory().remove(book);
                     bookstoreRepository.save(bookstore);
                 });
+    }
+
+    public void save(Bookstore bookstore) {
+        bookstoreRepository.save(bookstore);
+    }
+
+    public List<Bookstore> findAll() {
+        return bookstoreRepository.findAll();
+    }
+
+
+    public void deleteBookstore(Long id) {
+        var bookstore = bookstoreRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cannot find book."));
+        bookstoreRepository.delete(bookstore);
+    }
+
+    public void updateBookStore(Long id, String loc, Double price, Double money) {
+        if (Stream.of(loc, price, money).allMatch(Objects::isNull)) {
+            throw new UnsupportedOperationException("There is nothing to update");
+        }
+
+        var bookstore = bookstoreRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot find bookstore"));
+
+        if (loc != null) {
+            bookstore.setLocation(loc);
+        }
+
+        if (price != null) {
+            bookstore.setPriceModifier(price);
+        }
+
+        if (money != null) {
+            bookstore.setMoneyInCashRegister(money);
+        }
+
+        bookstoreRepository.save(bookstore);
     }
 }
---
+++ src/main/java/com/esen/bookstore/shell/BookstoreHandler.java
@@ -0,0 +1,58 @@
+package com.esen.bookstore.shell;
+
+import com.esen.bookstore.model.Book;
+import com.esen.bookstore.model.Bookstore;
+import com.esen.bookstore.service.BookstoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.standard.ShellCommandGroup;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ShellComponent
+@ShellCommandGroup("Bookstore related commands")
+@RequiredArgsConstructor
+public class BookstoreHandler {
+
+    private final BookstoreService bookstoreService;
+
+
+    @ShellMethod(value = "Create a bookstore", key = "create bookstore")
+    public void createBookstore(String location, Double priceModifier, Double moneyInCashRegister) {
+        bookstoreService.save(Bookstore.builder()
+                .location(location)
+                .priceModifier(priceModifier)
+                .moneyInCashRegister(moneyInCashRegister)
+                .build());
+    }
+
+    @ShellMethod(value = "List bookstores", key = "list bookstores")
+    public String listBookstores() {
+        return bookstoreService.findAll()
+                .stream()
+                .map(bookstore -> "Location: %s, Price modifier: %s, Money in Register: %f".formatted(
+                        bookstore.getId(),
+                        bookstore.getPriceModifier(),
+                        bookstore.getMoneyInCashRegister()
+                )).collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    @ShellMethod(value = "Delete bookstore", key = "delete book")
+    public void deleteBookstore(Long id) {
+        bookstoreService.deleteBookstore(id);
+    }
+
+    @ShellMethod(value = "Update bookstore", key = "update book")
+    public void updateBookstore(Long id,
+                           @ShellOption(defaultValue = ShellOption.NULL) String location,
+                           @ShellOption(defaultValue = ShellOption.NULL) Double price,
+                           @ShellOption(defaultValue = ShellOption.NULL) Double money
+                           ) {
+        bookstoreService.updateBookStore(id, location, price, money);
+    }
+
+
+}
